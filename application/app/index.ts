dedd72447871a732e708ad7c68eb940cd45abb14
GitGodDim <43316780+GitGodDim@users.noreply.github.com>
2022-09-26 17:48:06 +0300

    update prometheus server

--- application/app/index.ts
+++ application/app/index.ts
@@ -3,7 +3,7 @@ import 'express-async-errors';
 
 const app = express();
 
-app.get('*', (req: express.Request, res: express.Response) => {
+app.get('/', (req: express.Request, res: express.Response) => {
   const response = {
     hostname: req.hostname,
     uptime: process.uptime(),
@@ -15,4 +15,19 @@ app.get('*', (req: express.Request, res: express.Response) => {
 
 app.listen(3000, () => {
   console.log('listening on 3000');
+});
+
+// Use express as HTTP middleware
+// Feel free to use your own
+// var express = require('express')
+// var app = express()
+
+// Initialize Prometheus exporter
+const prom = require('prom-client')
+const prom_gc = require('prometheus-gc-stats')
+prom_gc()
+
+// Export Prometheus metrics from /metrics endpoint
+app.get('/metrics', function(req, res) {
+res.end(prom.register.metrics());
 });
